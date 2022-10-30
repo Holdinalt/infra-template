e8f1ecec42321944776063df665874f19248fd93
Holdest White <zbsholdest@mail.ru>
2022-10-30 21:26:02 +0300

    check 1

--- .github/scripts/tracker.js
+++ .github/scripts/tracker.js
@@ -13,31 +13,31 @@ const orgID = process.env.ID
 
 console.log(commits, 'commits')
 
-let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
-
-for(let commit of commits){
-    desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
-}
-
-const body = {
-    summary: tag,
-    description: desc
-}
-
-const xhr = new XMLHttpRequest();
-
-console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
-
-xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
-xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
-xhr.setRequestHeader('Authorization', `OAuth ${token}`)
-xhr.setRequestHeader('X-Org-ID', orgID)
-
-// xhr._onHttpResponse((req, res) => console.log(res))
-
-xhr.onreadystatechange = () => {
-    console.log(xhr.response)
-}
-
-xhr.send(JSON.stringify(body))
+// let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
+//
+// for(let commit of commits){
+//     desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
+// }
+//
+// const body = {
+//     summary: tag,
+//     description: desc
+// }
+//
+// const xhr = new XMLHttpRequest();
+//
+// console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
+//
+// xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
+// xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
+// xhr.setRequestHeader('Authorization', `OAuth ${token}`)
+// xhr.setRequestHeader('X-Org-ID', orgID)
+//
+// // xhr._onHttpResponse((req, res) => console.log(res))
+//
+// xhr.onreadystatechange = () => {
+//     console.log(xhr.response)
+// }
+//
+// xhr.send(JSON.stringify(body))
 
