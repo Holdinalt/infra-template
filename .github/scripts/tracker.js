63a7fa17f443429eecb54a196116b405b23c6d33
Holdest White <zbsholdest@mail.ru>
2022-10-30 21:10:20 +0300

    check tracker

--- .github/scripts/tracker.js
+++ .github/scripts/tracker.js
@@ -4,6 +4,8 @@ const token = process.env.TOKEN
 const ticket = process.env.TICKET
 const tag = process.env.TAG
 let commits = process.env.COMMITS
+// Когда сделал, понял что можно просто строку передавать уже отформатированную bash, а не массив.
+// Представьте, что этого костыля нет, пожалуйста. Мне жалко удалять эти 25 минут жизни
 commits = commits.slice(0, commits.length - 2) + commits.slice(commits.length - 1)
 const author = process.env.AUTHOR
 const orgID = process.env.ID
@@ -11,31 +13,31 @@ const orgID = process.env.ID
 console.log(JSON.parse(commits))
 console.log(JSON.parse(commits)[0])
 
-// let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
-//
-// for(let commit of commits){
-//     desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
-// }
-//
-// const body = {
-//     summary: tag,
-//     description: desc
-// }
-//
-// const xhr = new XMLHttpRequest();
-//
-// console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
-//
-// xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
-// xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
-// xhr.setRequestHeader('Authorization', `OAuth ${token}`)
-// xhr.setRequestHeader('X-Org-ID', orgID)
-//
-// // xhr._onHttpResponse((req, res) => console.log(res))
-//
-// xhr.onreadystatechange = () => {
-//     console.log(xhr.response)
-// }
-//
-// xhr.send(JSON.stringify(body))
+let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
+
+for(let commit of commits){
+    desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
+}
+
+const body = {
+    summary: tag,
+    description: desc
+}
+
+const xhr = new XMLHttpRequest();
+
+console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
+
+xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
+xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
+xhr.setRequestHeader('Authorization', `OAuth ${token}`)
+xhr.setRequestHeader('X-Org-ID', orgID)
+
+// xhr._onHttpResponse((req, res) => console.log(res))
+
+xhr.onreadystatechange = () => {
+    console.log(xhr.response)
+}
+
+xhr.send(JSON.stringify(body))
 
