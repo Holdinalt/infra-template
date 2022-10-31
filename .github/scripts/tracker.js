14d7ce33d1274e3860741ef97dd1af645e205c81
Holdest White <zbsholdest@mail.ru>
2022-10-31 03:48:20 +0300

    final cut

--- .github/scripts/tracker.js
+++ .github/scripts/tracker.js
@@ -13,31 +13,43 @@ const orgID = process.env.ID
 
 console.log(commits, 'commits')
 
-// let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
-//
-// for(let commit of commits){
-//     desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
-// }
-//
-// const body = {
-//     summary: `Релиз ${tag} - ${new Date().toLocaleDateString('en-US')}`,
-//     description: desc
-// }
-//
-// const xhr = new XMLHttpRequest();
-//
-// console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
-//
-// xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
-// xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
-// xhr.setRequestHeader('Authorization', `OAuth ${token}`)
-// xhr.setRequestHeader('X-Org-ID', orgID)
-//
-// // xhr._onHttpResponse((req, res) => console.log(res))
-//
-// xhr.onreadystatechange = () => {
-//     console.log(xhr.response)
-// }
-//
-// xhr.send(JSON.stringify(body))
+let desc = `Ответственный за релиз ${author}.\n Коммиты, попавшие в релиз:\n`
+
+for(let commit of commits){
+    desc += (`${commit[0]} ${commit[1]} ${commit[2]}\n`)
+}
+
+const body = {
+    summary: `Релиз ${tag} - ${new Date().toLocaleDateString('en-US')}`,
+    description: desc
+}
+
+const xhr = new XMLHttpRequest();
+
+console.log(`https://api.tracker.yandex.net/v2/issues/${ticket}`)
+
+xhr.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}`)
+xhr.setRequestHeader('Host', 'https://api.tracker.yandex.net')
+xhr.setRequestHeader('Authorization', `OAuth ${token}`)
+xhr.setRequestHeader('X-Org-ID', orgID)
+
+// xhr._onHttpResponse((req, res) => console.log(res))
+
+xhr.onreadystatechange = () => {
+    console.log(xhr.response)
+}
+
+xhr.send(JSON.stringify(body))
+
+const xhrComment = new XMLHttpRequest();
+xhrComment.open("PATCH", `https://api.tracker.yandex.net/v2/issues/${ticket}/comments`)
+xhrComment.setRequestHeader('Host', 'https://api.tracker.yandex.net')
+xhrComment.setRequestHeader('Authorization', `OAuth ${token}`)
+xhrComment.setRequestHeader('X-Org-ID', orgID)
+
+xhrComment.onreadystatechange = () => {
+    console.log(xhrComment.response)
+}
+
+xhrComment.send(JSON.stringify({text: `Собрали образ в тегом ${tag}`}))
 
